425f0323ee7969e012db0315394c85e25fb13b36
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-19 10:05:22 +0100

    Simple round robin example

---
+++ round_robin.py
@@ -0,0 +1,22 @@
+import itertools
+from random import randint, choice
+from time import sleep
+
+processList = ["proc1", "proc2", "proc3", "proc4", "proc5"]
+jobsList = ["job1", "job2", "job3", "job4", "job5", "job6", "job7", "job8", "job9", "job10"]
+doneJobs = []
+roundRobin = itertools.cycle(processList)
+
+def executeJob(job, processToExecute, assignedProcessorTime):
+    print("Job " + job + " is beeing executed by " + processToExecute)
+    sleep(assignedProcessorTime)
+    print("Job " + job + " executed by " + processToExecute)
+
+while len(doneJobs) < len(jobsList):
+    processToExecute = roundRobin.next()
+    jobTobeExecuted = choice(jobsList)
+    assignedProcessorTime = randint(0, 10)
+    executeJob(jobTobeExecuted, processToExecute, assignedProcessorTime)
+    doneJobs.append(jobTobeExecuted)
+
+
