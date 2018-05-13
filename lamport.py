6a3e89784ed38f775a89673deb408915cfe7596a
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-05-13 22:16:16 +0200

    Refactoring lamport algorithm

--- lamport.py
+++ lamport.py
@@ -9,34 +9,61 @@ processesCorrect = {'P1': {'a': 1, 'b': 2, 'c': 3, 'd': 4},
 messages = {'h': 'b', 'c': 'f', 'd': 'i'}
 
 
+def determineSenderClock(process, sendOperation):
+    senderClock = None
+    i = 0
+    while senderClock is None and i < len(processesWrong[process]) - 1:
+        senderClock = processesWrong[process].get(sendOperation)
+        i += 1
+    return senderClock
+
+
+def determineReceiverClock(process, receiveOperation):
+    receiverClock = None
+    i = 0
+    while receiverClock is None and i < len(processesWrong[process]) - 1:
+        receiverClock = processesWrong[process].get(receiveOperation)
+        i += 1
+    return receiverClock
+
+
+def correctReceiverClock(receiveOperation, senderClock):
+    for p in processesWrong:
+        if processesWrong[p].get(receiveOperation) is not None:
+            processesCorrect[p][receiveOperation] = senderClock + 1
+
+
+def correctLocalOrdering(process):
+    previousValue = list(processesCorrect[process].values())[0] - 1
+    for key, value in processesCorrect[process].items():
+        if value <= previousValue:
+            processesCorrect[process][key] = previousValue + 1
+        previousValue = value
+
+
 def correctOrderingAccordingToLamportRules():
-    for key, value in messages.items():
+    # iterate through messages in the system
+    for sendOperation, receiveOperation in messages.items():
         senderClock = None
         receiverClock = None
 
-        for p in processesWrong:
-
+        # iterate through processes
+        for process in processesWrong:
             i = 0
-            while senderClock is None and i < len(processesWrong[p]) - 1:
-                senderClock = processesWrong[p].get(key)
+            while senderClock is None and i < len(processesWrong[process]) - 1:
+                senderClock = processesWrong[process].get(sendOperation)
                 i += 1
 
             i = 0
-            while receiverClock is None and i < len(processesWrong[p]) - 1:
-                receiverClock = processesWrong[p].get(value)
+            while receiverClock is None and i < len(processesWrong[process]) - 1:
+                receiverClock = processesWrong[process].get(receiveOperation)
                 i += 1
 
         if receiverClock <= senderClock:
-            for p in processesWrong:
-                if processesWrong[p].get(value) is not None:
-                    processesCorrect[p][value] = senderClock + 1
+            correctReceiverClock(receiveOperation, senderClock)
 
     for p in processesCorrect:
-        previousValue = list(processesCorrect[p].values())[0] - 1
-        for key, value in processesCorrect[p].items():
-            if (value <= previousValue):
-                processesCorrect[p][key] = previousValue + 1
-            previousValue = value
+        correctLocalOrdering(p)
 
 
 print('Processes wrong ordering ' + str(processesWrong))
